46f92a6694aaa1db6c956242377f257362ffa87d
Giacomo Cui <giacomocui2012@gmail.com>
2023-02-05 11:30:11 +0100

    commit per far funzionare su MACCATALYST

--- Helpers/ServiceHelper.cs
+++ Helpers/ServiceHelper.cs
@@ -7,14 +7,16 @@ public static class ServiceHelper
         Current.GetService<TService>();
 
     public static IServiceProvider Current =>
-    #if WINDOWS10_0_17763_0_OR_GREATER
+#if WINDOWS10_0_17763_0_OR_GREATER
             MauiWinUIApplication.Current.Services;
-    #elif ANDROID
+#elif ANDROID
             MauiApplication.Current.Services;
-    #elif IOS || MACCATALIST
+#elif IOS
             MauiUIApplicationDelegate.Current.Services;
-    #else
+#elif MACCATALYST
+        MauiUIApplicationDelegate.Current.Services;
+#else
             null;
-    #endif
+#endif
 
 }
