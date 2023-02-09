52672da4b71ca6471a5f1ef40958e36a2501ec9b
Nova <mce_asterisks@aleeas.com>
2023-02-09 11:14:52 +0100

    Creata la Shell dell'applicazione, con collegamenti a LoginPage, a StartPage, a TournamentsListPage, a GeneralSettingsPage e ad AppearancePage.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using JOIN.Views;
+namespace JOIN
+{
+    public partial class AppShell : Microsoft.Maui.Controls.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            /*Routing.RegisterRoute(nameof(AddTournamentPage),
+                typeof(AddTournamentPage));
+            */
+            Routing.RegisterRoute(nameof(TournamentPage),
+                typeof(TournamentPage));
+
+            Routing.RegisterRoute(nameof(RegistrationPage),
+                typeof(RegistrationPage));
+        }
+    }
+}
